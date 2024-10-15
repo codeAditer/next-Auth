b18bbd0d30f6627d2a905742d33e4b142d94b588
Aditya <adityasha0919@gmail.com>
2024-10-15 23:30:01 +0530

    addd auth using clerk and complete the basic ui part

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Link from 'next/link'
+import { SignInButton,SignedIn,SignedOut,UserButton } from '@clerk/nextjs'
 
 const Header = () => {
   return (
-  <header className=' shadow-lg'>
+  <header className='bg-black text-white shadow-lg'>
     <div className='max-w-6xl mx-auto flex justify-between items-center p-3'>
+     {/* logo */}
     <Link href='/' className='text-2xl font-extrabold group cursor-pointer'>
           <span className='bg-clip-text bg-gradient-to-r from-gray-900 to-gray-600 group-hover:from-gray-800 group-hover:to-gray-500 transition-colors duration-300'>
             Auth
@@ -13,12 +15,17 @@ const Header = () => {
             App
           </span>
         </Link>
-
+            {/* add a navigation menu */}
         <nav>
             <ul className='flex gap-4 '>
-                <Link href ='/'>Home</Link>
-                <Link href ='/about'>About</Link>
-                <Link href ='/signin'>SignIn</Link>
+                <Link className='text-sm' href ='/'>Home</Link>
+                <Link className='text-sm'  href ='/about'>About</Link>
+                <SignedIn className='text-sm'>
+              <UserButton />
+            </SignedIn>
+            <SignedOut>
+                <SignInButton/>
+            </SignedOut>
             </ul>
         </nav>
     </div>
